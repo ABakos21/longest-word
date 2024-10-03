fa98fdea97aa7cfcd2f2a62b9f2f99a9e6652f27
Andrew Bakos <andrew@romutrechtregion.nl>
2024-10-03 18:19:54 +0000

    Update

--- longest_word/game.py
+++ longest_word/game.py
@@ -6,7 +6,7 @@ class Game:
         """Initialize a random grid of size 9"""
         self.grid = self.generate_random_grid()
 
-    def generate_random_grid1(self) -> list:
+    def generate_random_grid(self) -> list:
         """Generate a random 3x3 grid of uppercase letters"""
         return [random.choice(string.ascii_uppercase) for _ in range(9)]
 
